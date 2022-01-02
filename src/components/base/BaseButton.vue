4962966257f560bdbed41d6a52d6af1a7220dc0f
Justin Shilling <loveofmusic25@hotmail.com>
2022-01-02 10:18:00 -0500

    converted BaseButton to ts

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,96 +1,113 @@
 <template>
-    <button
-        :class="['button', { hovering: isHovering && showHoverEffect }]"
-        :style="styleObject"
-        @mouseover="isHovering = true"
-        @mouseleave="isHovering = false"
-    >
-        <slot>Button</slot>
-    </button>
+  <button
+    :class="['button', { hovering: isHovering && showHoverEffect }]"
+    :style="styleObject"
+    @mouseover="isHovering = true"
+    @mouseleave="isHovering = false"
+  >
+    <slot>Button</slot>
+  </button>
 </template>
 
-<script>
-export default {
-    name: "BaseButton",
-    props: {
-        backgroundColor: {
-            type: String,
-            default: "button-primary-bgcolor",
-        },
-        border: {
-            type: String,
-            default: "#fff",
-        },
-        borderRadius: {
-            type: String,
-            default: "30px",
-        },
-        color: {
-            type: String,
-            default: "button-primary-color",
-        },
-        fontSize: {
-            type: String,
-            default: "16px",
-        },
-        fontWeight: {
-            type: String,
-            default: "600",
-        },
-        minWidth: {
-            type: String,
-            default: "180px",
-        },
-        padding: {
-            type: String,
-            default: "14px 0",
-        },
-        showHoverEffect: {
-            type: Boolean,
-            default: true,
-        },
-        showShadow: {
-            type: Boolean,
-            default: false,
-        },
-        dropShadow: {
-            type: String,
-            default: "0 13px 15px -13px rgb(242, 95, 58)"
-        }
-    },
-    data() {
-        return {
-            styleObject: {
-                backgroundColor: `var(--${this.backgroundColor})`,
-                border: this.border,
-                borderRadius: this.borderRadius,
-                boxShadow: this.showShadow
-                    ? `var(--${this.dropShadow})`
-                    : "",
-                color: `var(--${this.color})`,
-                fontSize: this.fontSize,
-                fontWeight: this.fontWeight,
-                minWidth: this.minWidth,
-                padding: this.padding,
-            },
-            isHovering: false,
-        };
-    },
-};
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Options({})
+export default class BaseButton extends Vue {
+  @Prop({
+    type: String,
+    default: "button-primary-bgcolor"
+  })
+  backgroundColor!: string;
+
+  @Prop({
+    type: String,
+    default: "#fff"
+  })
+  border!: string;
+
+  @Prop({
+    type: String,
+    default: "30px"
+  })
+  borderRadius!: string;
+
+  @Prop({
+    type: String,
+    default: "button-primary-color"
+  })
+  color!: string;
+
+  @Prop({
+    type: String,
+    default: "16px"
+  })
+  fontSize!: string;
+
+  @Prop({
+    type: String,
+    default: "600"
+  })
+  fontWeight!: string;
+
+  @Prop({
+    type: String,
+    default: "180px"
+  })
+  minWidth!: string;
+
+  @Prop({
+    type: String,
+    default: "14px 0"
+  })
+  padding!: string;
+
+  @Prop({
+    type: Boolean,
+    default: true
+  })
+  showHoverEffect!: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  showShadow!: boolean;
+
+  @Prop({
+    type: String,
+    default: "0 13px 15px -13px rgb(242, 95, 58)"
+  })
+  dropShadow!: string;
+
+  private styleObject = {
+    backgroundColor: `var(--${this.backgroundColor})`,
+    border: this.border,
+    borderRadius: this.borderRadius,
+    boxShadow: this.showShadow ? `var(--${this.dropShadow})` : "",
+    color: `var(--${this.color})`,
+    fontSize: this.fontSize,
+    fontWeight: this.fontWeight,
+    minWidth: this.minWidth,
+    padding: this.padding
+  };
+  private isHovering = false;
+}
 </script>
 
 <style lang="scss" scoped>
 .button {
-    font-family: "Be Vietnam", sans-serif;
-    outline: none;
-    letter-spacing: 1px;
-    text-align: center;
-    cursor: pointer;
-    transition: background-color 0.3s, letter-spacing 0.3s;
-    &.hovering {
-        background-color: #333;
-        color: #fff !important;
-        letter-spacing: 1.4px;
-    }
+  font-family: "Be Vietnam", sans-serif;
+  outline: none;
+  letter-spacing: 1px;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.3s, letter-spacing 0.3s;
+  &.hovering {
+    background-color: #333;
+    color: #fff !important;
+    letter-spacing: 1.4px;
+  }
 }
 </style>
