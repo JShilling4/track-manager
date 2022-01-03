a48aed2f71b09c59cddc0b14f50d72cd9d75999c
Justin Shilling <loveofmusic25@hotmail.com>
2022-01-02 19:01:55 -0500

    config adjustment

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,31 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-	configureWebpack: {
-		devtool: !isProduction ? "source-map" : undefined
-	},
-	css: {
-		loaderOptions: {
-			sass: {
-				prependData: `
+  configureWebpack: {
+    devtool: !isProduction ? "source-map" : undefined
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
 					@import "@/design/scss/mixins.scss";
-				`,
-			},
-		},
-		sourceMap: true,
-	},
+				`
+      }
+    },
+    sourceMap: true
+  },
 
-	devServer: {
-		host: "localhost",
-	},
+  devServer: {
+    host: "localhost"
+  },
+
+  chainWebpack: (config) => {
+    if (isProduction) {
+      config.optimization.minimizer("terser").tap((args) => {
+        const { terserOptions } = args[0];
+        terserOptions["keep_classnames"] = true;
+        return args;
+      });
+    }
+  }
 };
