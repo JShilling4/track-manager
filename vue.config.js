06f8db4dfebfc512557b0a4311584d0f1e16fd96
Justin Shilling <loveofmusic25@hotmail.com>
2022-01-02 19:05:47 -0500

    config adjustment

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,8 @@ module.exports = {
     if (isProduction) {
       config.optimization.minimizer("terser").tap((args) => {
         const { terserOptions } = args[0];
-        terserOptions["keep_classnames"] = true;
+				terserOptions["keep_classnames"] = true;
+				terserOptions["keep_fnames"] = true;
         return args;
       });
     }
